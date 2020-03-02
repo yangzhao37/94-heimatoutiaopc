ece838942066bfd2080fe1dfb2cae3d58d44ff5d
yangzhao <yangzhao@outlook.com>
2020-03-02 22:32:38 +0800

    黑马头条PC-登录模块-页面布局及样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,36 @@
 <template>
-  <div>
-      <h1>我是登录页</h1>
+  <div class="login">
+      <!-- 表单 -->
+      <el-card class="login-card">
+          <!-- 表单内容 -->
+          <!-- 头部logo部分 -->
+          <div class="title">
+              <img src="../../assets/img/logo_index.png" alt="">
+          </div>
+          <!-- 表单 -->
+          <el-form>
+              <!-- 表单容器 -->
+              <el-form-item>
+                  <!-- 表单域 -->
+                  <el-input placeholder="请输入手机号"></el-input>
+              </el-form-item>
+              <!-- 验证码 -->
+              <el-form-item>
+                  <el-input style="width:60%" placeholder="请输入验证码"></el-input>
+                  <!-- 放置一个按钮 -->
+                  <el-button style="float:right">发送验证码</el-button>
+              </el-form-item>
+              <!-- 表单域 -->
+              <el-form-item>
+                  <!-- 是否同意该协议规则 -->
+                  <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
+              </el-form-item>
+              <!-- 登录按钮 -->
+              <el-form-item>
+                  <el-button style="width:100%" type="primary">登录</el-button>
+              </el-form-item>
+          </el-form>
+      </el-card>
   </div>
 </template>
 
@@ -10,6 +40,25 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+//加了scoped属性 就只会对当前自己的组件起作用
+//如果需要写less 需要在style标签中 lang='less'
+.login {
+    background-image: url('../../assets/img/abc.jpg');
+    height: 100vh;//当前屏幕可视区域分成100份
+    background-size: cover;//自适应当前的屏幕区
+    display:flex;
+    justify-content: center;//水平居中
+    align-items: center;//垂直居中
+    .login-card{
+        width: 440px;
+        height: 340px;
+    }
+    .title{
+        text-align: center;
+        img {
+            height: 40px;
+        }
+    }
+}
 </style>
